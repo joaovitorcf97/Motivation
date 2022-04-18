efc9525d40a6b5e6983d5e5e2fbc2938cf33861f
Joao Vitor <joaovitorcfdev@gmail.com>
2022-04-18 17:03:03 -0300

    navigating to home page

--- app/src/main/java/com/example/motivation/UserActivity.kt
+++ app/src/main/java/com/example/motivation/UserActivity.kt
@@ -1,8 +1,10 @@
 package com.example.motivation
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import com.example.motivation.databinding.ActivityUserBinding
 
 class UserActivity : AppCompatActivity(), View.OnClickListener {
@@ -12,6 +14,7 @@ class UserActivity : AppCompatActivity(), View.OnClickListener {
         super.onCreate(savedInstanceState)
 
         binding = ActivityUserBinding.inflate(layoutInflater)
+        binding.buttonSave.setOnClickListener(this)
         supportActionBar?.hide()
 
         setContentView(binding.root)
@@ -19,7 +22,17 @@ class UserActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onClick(v: View) {
         if (v.id == R.id.button_save) {
-            
+            handleSave()
+        }
+    }
+
+    private fun handleSave() {
+        val name = binding.editName.text.toString()
+        if (name != "") {
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        } else {
+            Toast.makeText(this, R.string.validation_mandatory_name, Toast.LENGTH_SHORT).show()
         }
     }
 }
